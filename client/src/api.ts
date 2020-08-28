92d3b5d1e4f34cefd8cadff693bb36018a0f2d45
Athman Gude <athmangude@gmail.com>
2020-08-28 07:44:25 +0300

    added code to communicate with api

---
+++ client/src/api.ts
@@ -0,0 +1,53 @@
+import axios, { AxiosResponse } from 'axios';
+import Axios from 'axios';
+
+const baseUrl: string = 'http://localhost:4000';
+
+export const getTodos = async (): Promise<AxiosResponse<ApiDataType>> => {
+  try {
+    const todos: AxiosResponse<ApiDataType> = await axios.get(`${baseUrl}/todos`);
+    return todos;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function addTodo(formData: ITodo): Promise<AxiosResponse<ApiDataType>> {
+  try {
+    const todo: Omit<ITodo, '_id'> = {
+      name: formData.name,
+      description: formData.description,
+      status: false,
+    }
+
+    const saveTodo: AxiosResponse<ApiDataType> = await axios.post(`${baseUrl}/todos`, todo);
+    return saveTodo;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function updateTodo(todo: ITodo): Promise<AxiosResponse<ApiDataType>> {
+  // only update the status of the todo
+  try {
+    const todoUpdate: Pick<ITodo, 'name', 'description', 'status'> = {
+      name: todo.name,
+      description: todo.description,
+      status: true,
+    }
+
+    const updatedTodo: AxiosResponse<ApiDataType> = await Axios.put(`${baseUrl}/todos/${todo._id}`, todoUpdate);
+    return updatedTodo;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export async function deleteTodo(_id: string): Promise<AxiosResponse<ApiDataType>> {
+  try {
+    const deletedTodo: AxiosResponse<ApiDataType> = await Axios.delete(`${baseUrl}/todos/${_id}`);
+    return deletedTodo;
+  } catch (error) {
+    throw new Error(error);
+  }
+}
